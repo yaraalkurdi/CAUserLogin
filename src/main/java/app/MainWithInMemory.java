7379c8eba7a2a671a880d31bc33c15043c750429
Yara Alkurdi <yara.alkurdi@mail.utoronto.ca>
2024-10-24 22:23:57 -0400

    "Fixing Checkstyle errors"

--- src/main/java/app/MainWithInMemory.java
+++ src/main/java/app/MainWithInMemory.java
@@ -1,8 +1,10 @@
 package app;
 
-import data_access.DBUserDataAccessObject;
+import java.awt.*;
+
+import javax.swing.*;
+
 import data_access.InMemoryUserDataAccessObject;
-import entity.CommonUserFactory;
 import interface_adapter.ViewManagerModel;
 import interface_adapter.logged_in.LoggedInViewModel;
 import interface_adapter.login.LoginViewModel;
@@ -11,9 +13,6 @@ import view.LoggedInView;
 import view.LoginView;
 import view.SignupView;
 import view.ViewManager;
-
-import javax.swing.*;
-import java.awt.*;
 
 /**
  * The version of Main with an external database used to persist user data.
