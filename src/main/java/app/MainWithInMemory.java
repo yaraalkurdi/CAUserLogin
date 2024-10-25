46d82a9ab71436985692d4d7c724ce6a5fd9e0d4
Yara Alkurdi <yara.alkurdi@mail.utoronto.ca>
2024-10-24 22:27:18 -0400

    "Fixed Checkstyle flags"

--- src/main/java/app/MainWithInMemory.java
+++ src/main/java/app/MainWithInMemory.java
@@ -1,8 +1,10 @@
 package app;
 
-import java.awt.*;
+import java.awt.CardLayout;
 
-import javax.swing.*;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.WindowConstants;
 
 import data_access.InMemoryUserDataAccessObject;
 import interface_adapter.ViewManagerModel;
